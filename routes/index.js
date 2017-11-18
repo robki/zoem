c127b195331ea63834c910b05c9d7a15708d8bf2
robki <robin@ruddervoorde.be>
2017-11-18 10:33:20 +0100

    added routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var path  =require("path");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendFile(path.join(__dirname,"../public","/index.html"));
+});
+
+router.get('/about', function(req, res, next) {
+  res.sendFile(path.join(__dirname,"../public","/about.html"));
+});
+
+router.get('/register', function(req, res, next) {
+  res.sendFile(path.join(__dirname,"../public","/register.html"));
 });
 
 module.exports = router;
