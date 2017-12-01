cfc9407a2738d0dc19b0948d26b678d20536fa7c
joffreymahieu <joffrey-mahieu@hotmail.com>
2017-12-01 13:36:57 +0100

    delete old project

--- config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var config = {
-    HOST: 'http://localhost',
-    PORT:  9696,
-    MONGODBURL : process.env.MONGO_URI || 'mongodb://localhost/zoem',
-};
-
-module.exports = config;
