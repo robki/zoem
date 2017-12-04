c63d1e5fa8901a275a9889ebefd50ade9d71d687
joffreymahieu <joffrey-mahieu@hotmail.com>
2017-12-04 13:12:20 +0100

    routes & start of signup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { PageComponent } from './page/page.component';
 import { CarsresultComponent } from './carsresult/carsresult.component';
+import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
   {path: '', pathMatch: 'full', redirectTo: 'page'},
   {path: 'about', component: AboutComponent},
+  {path: 'signup', component: SignupComponent},
   {path: '', component: PageComponent},
   {path: 'carsresult', component: CarsresultComponent}
+
 
 ];
 
