377c3e00459481d62534f2ca31f8910aa44277d0
robki <robin@ruddervoorde.be>
2017-12-08 15:40:33 +0100

    updata api

--- server/routes/api.js
+++ server/routes/api.js
@@ -3,9 +3,61 @@
 const express = require('express');
 const router = express.Router();
 const users = require("../../data/models/usersRepo");
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
 
+//Connect
+const connection = (closure) => {
+    return MongoClient.connect('mongodb://localhost:27017/zoem', (err, db) => {
+        if (err) return console.log(err);
 
-/* GET api listing. */
-router.get('/', users.getAllUsers);
+        closure(db);
+    });
+};
+
+// Error handling
+const sendError = (err, res) => {
+    response.status = 501;
+    response.message = typeof err == 'object' ? err.message : err;
+    res.status(501).json(response);
+};
+
+// Response handling
+let response = {
+    status: 200,
+    data: [],
+    message: null
+};
+
+// Get users
+router.get('/users', (req, res) => {
+    connection((db) => {
+        db.collection('users')
+            .find()
+            .toArray()
+            .then((users) => {
+                response.data = users;
+                res.json(response);
+            })
+            .catch((err) => {
+                sendError(err, res);
+            });
+    });
+});
+
+router.get('/cars', (req, res) => {
+    connection((db) => {
+        db.collection('cars')
+            .find()
+            .toArray()
+            .then((users) => {
+                response.data = users;
+                res.json(response);
+            })
+            .catch((err) => {
+                sendError(err, res);
+            });
+    });
+});
 
 module.exports = router;
