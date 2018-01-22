054d8ac8cd010a47ec7a5705cd7588b565947be0
joffreymahieu <joffrey-mahieu@hotmail.com>
2018-01-22 14:14:52 +0100

    header and footer update with login + profilepicture by account

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthService} from './../auth/auth.service';
 @Component({
   selector: 'zm-account',
   templateUrl: './account.component.html',
@@ -7,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountComponent implements OnInit {
 
-  constructor() { }
+  constructor(public auth: AuthService) {
+    
+  }
+
 
   ngOnInit() {
    
--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,10 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService} from './../auth/auth.service';
 @Component({
   selector: 'zm-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
+  constructor(public auth: AuthService) {
+    
+  }
+
   //show registercomponent on click in footer
   open():void{
     (<HTMLInputElement>document.getElementById('modal')).style.display = 'block';
