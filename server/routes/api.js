3656e0dc79f0962ec302a4f128ec5143fba4486f
robki <robin@ruddervoorde.be>
2018-01-25 20:35:45 +0100

    cleanup api & model/schema

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,50 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const MongoClient = require('mongodb').MongoClient;
-const ObjectID = require('mongodb').ObjectID;
-var bodyParser = require('body-parser');
 var User = require('../data/models/user');
-router.use(bodyParser.urlencoded({
-  extended: true
-}));
-
-
-//Connect
-const connection = (closure) => {
-  return MongoClient.connect('mongodb://zoem:zoem123456@ds251727.mlab.com:51727/zoem', (err, db) => {
-    if (err) return console.log(err);
-
-    closure(db);
-  });
-};
-
-// Error handling
-const sendError = (err, res) => {
-  response.status = 501;
-  response.message = typeof err == 'object' ? err.message : err;
-  res.status(501).json(response);
-};
-
-// Response handling
-let response = {
-  status: 200,
-  data: [],
-  message: null
-};
 
 // Get Users
-router.get('/getusers', (req, res) => {
-  connection((db) => {
-    db.collection('users')
-      .find()
-      .toArray()
-      .then((cars) => {
-        response.data = cars;
-        res.json(cars);
-      })
-      .catch((err) => {
-        sendError(err, res);
-      });
+router.get('/getusers', function(req,res,next){  
+  User.getAllUsers(function(err,results){
+    if(err) console.log(err);
+    var users = results;
+    res.json(users);
+    console.log(users);
   });
 });
 
