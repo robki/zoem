042fb7995d97788286abc2435d6d4a5ff7f121ab
joffreymahieu <joffrey-mahieu@hotmail.com>
2018-01-26 14:04:55 +0100

    final push

--- config.js
+++ config.js
@@ -3,7 +3,8 @@
 var config = {
     HOST: 'http://localhost',
     PORT:  3000,
-    MONGODBURL : process.env.MONGO_URI || 'mongodb://zoem:zoem123456@ds251727.mlab.com:51727/zoem',
+    MONGODBURL : process.env.MONGO_URI || 'mongodb://localhost/zoem2',
+    //'mongodb://zoem:zoem123456@ds251727.mlab.com:51727/zoem'
 };
 
 module.exports = config;
